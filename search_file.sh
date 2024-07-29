0129fb85925e712797e76f6df2cb04f3ee7b4c84
Chanpreetkaur20 <chanpreetkaur2005@gmail.com>
2024-07-30 02:37:51 +0530

    Create search_file.sh

---
+++ search_file.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+dir="$1"
+filename="$2"
+
+search_file() {
+    for item in "$1"/*
+    do
+        if [ -d "$item" ]
+        then
+            search_file "$item" "$2"
+        elif [ -f "$item" ]
+        then
+            if [ "$(basename "$item")" = "$2" ]
+            then
+                echo "File found: $item"
+            fi
+        fi
+    done
+}
+
+search_file "$dir" "$filename"
